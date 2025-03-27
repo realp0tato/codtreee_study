76cc0f0329a0f4021e27e469e7f36e017fac5de8
realp0tato <136218419+realp0tato@users.noreply.github.com>
2025-03-27 17:01:57 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250327/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+
+void print_star(int n)
+{
+    if(n==0)
+        return;
+
+    for(int i = 1; i <= n; i ++)
+        cout << "*" << " ";
+    cout << endl;
+    print_star(n-1);
+    for(int i = 1; i <= n; i++)
+        cout << "*" << " ";
+    cout << endl;
+}
+
+int main() {
+    cin >> n;
+
+    print_star(n);
+
+    return 0;
+}
