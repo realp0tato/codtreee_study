e8f753f5c9609553c91fac152b4a153952677c1c
realp0tato <136218419+realp0tato@users.noreply.github.com>
2025-03-27 17:03:18 +0900

    Title: 이상한 수열, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250327/이상한 수열/a-strange-sequence.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int f(int n)
+{
+    if(n == 1)
+        return 1;
+    if(n == 2)
+        return 2;
+
+    return f(n/3) + f(n-1);
+}
+
+int main() {
+    cin >> N;
+
+    cout << f(N);
+    return 0;
+}
