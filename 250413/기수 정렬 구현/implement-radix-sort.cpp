2485bf1dc9e7b0c1765f22e9664980bb9aac6d86
realp0tato <136218419+realp0tato@users.noreply.github.com>
2025-04-13 02:34:03 +0900

    Title: 기수 정렬 구현, Time: 41ms, Memory: 1MB, Status: Passed - Codetree

--- 250413/기수 정렬 구현/implement-radix-sort.cpp
+++ 250413/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 using namespace std;
 
 int n;
@@ -8,26 +9,21 @@ int arr[100000];
 
 void radix_sort(int k)
 {
-    vector<int> bucket[10];
-    int div = 1;
+    //기수 정렬, k는 자릿수
+    vector<int> bucket[10]; // 0~9까지의 버킷
     for (int i = 0; i < k; i++) {
         for (int j = 0; j < n; j++) {
-            int digit = (arr[j] / div) % 10;
-            bucket[digit].push_back(arr[j]);
+            int digit = (arr[j] / (int)pow(10, i)) % 10; // i번째 자릿수
+            bucket[digit].push_back(arr[j]); // 해당 자릿수에 맞는 버킷에 넣기
         }
         int index = 0;
         for (int j = 0; j < 10; j++) {
             for (int l = 0; l < bucket[j].size(); l++) {
-                arr[index++] = bucket[j][l];
+                arr[index++] = bucket[j][l]; // 버킷에서 꺼내서 원래 배열에 넣기
             }
-            bucket[j].clear();
+            bucket[j].clear(); // 버킷 초기화
         }
-        div *= 10;
     }
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-
 }
 
 int main() {
@@ -41,9 +37,12 @@ int main() {
     }
     while(max > 0)
     {
-        max %= 10;
+        max /= 10;
         k++;
     }
     radix_sort(k);
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
     return 0;
 }
