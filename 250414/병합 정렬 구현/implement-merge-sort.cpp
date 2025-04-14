77e076685da1f31f5b6e6c9f5b5fd9d0a8658ea6
realp0tato <136218419+realp0tato@users.noreply.github.com>
2025-04-14 20:31:49 +0900

    Title: 병합 정렬 구현, Time: 37ms, Memory: 1MB, Status: Passed - Codetree

---
+++ 250414/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,104 @@
+// #include <iostream>
+
+// using namespace std;
+
+// int gcd(int a, int b){
+//     int n;
+//     while(b!=0){
+//         n=a%b;
+//         a=b;
+//         b=n;
+//     }
+//     return a;
+// }
+
+// int main()
+// {
+//     int n, m;
+    
+//     cin >> n >> m;
+    
+//     cout << gcd(n, m) << endl;
+
+//     return 0;
+// }
+
+
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100000];
+int merge_arr[100000];
+void merge(int low, int m, int high);
+void merge_sort(int low, int high);
+int main()
+{
+    cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    
+
+    int high = n-1;
+    int low = 0;
+    merge_sort(low, high);
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
+
+void merge_sort(int low, int high)
+{   if(low < high)
+    {
+        int mid = (low + high) / 2;
+        merge_sort(low, mid);
+        merge_sort(mid + 1, high);
+        merge(low, mid, high);
+        
+    }
+
+}
+
+void merge(int low, int m, int high)
+{
+    int i = low;
+    int j = m + 1;
+    int k = low;
+
+    while(i <= m && j <= high)
+    {
+        if(arr[i] <= arr[j])
+        {
+            merge_arr[k] = arr[i];
+            k++;
+            i++;
+        }
+        else
+        {
+            merge_arr[k] = arr[j];
+            k++;
+            j++;
+        }
+    }
+    while(i <= m)
+    {
+        merge_arr[k] = arr[i];
+        k++;
+        i++;
+    }
+    while(j <= high)
+    {
+        merge_arr[k] = arr[j];
+        k++;
+        j++;
+    }
+    for(int k = low; k<=high; k++)
+    {
+        arr[k] = merge_arr[k];
+    }
+}
+
